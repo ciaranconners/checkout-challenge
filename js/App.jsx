82348d7c5d55f7ce88d252e62c93cc37f1c41ab2
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-12-08 15:20:47 -0500

    separates inputform into a separate component, makes code surrounding form submission and comment saving DRYer

--- js/App.jsx
+++ js/App.jsx
@@ -1,51 +1,25 @@
 import React from 'react';
+import InputForm from './InputForm';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       comments: [],
-      input: {
-        name: '',
-        email: '',
-        rating: '',
-        text: '',
-      },
     };
-    this.onClick = this.onClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.saveComment = this.saveComment.bind(this);
   }
 
-  onClick() {
-    const newComment = this.state.input;
+  saveComment(newComment) {
     const comments = this.state.comments;
     comments.push(newComment);
-    this.setState({comments, input: {name: '', email: '', rating: '', text: ''}});
-    document.getElementById('comment-input').reset();
-  }
-
-  handleChange(e) {
-    const field = e.target.name;
-    const value = e.target.value;
-    const input = this.state.input;
-    input[field] = value;
-    this.setState({input});
+    this.setState({comments});
   }
 
   render() {
     return (
       <div>
-        <form id='comment-input'>
-          Name: <br/>
-          <input name="name" type="text" value={this.state.comments.name} onChange={this.handleChange}/><br/>
-          Email: <br/>
-          <input name="email" type="text" value={this.state.comments.email} onChange={this.handleChange}/><br/>
-          Rating: <br/>
-          <input name="rating" type="text" value={this.state.comments.rating} onChange={this.handleChange}/><br/>
-          Comment: <br/>
-          <input name="text" type="text" value={this.state.comments.text} onChange={this.handleChange}/><br/>
-          <input type="button" value="Submit" onClick={this.onClick}/>
-        </form>
+        <InputForm saveComment={this.saveComment}/>
         <div id='comment-display'>
           Recent Comments:<br/>
           {this.state.comments.map((comment) => <div id='comment' key={comment.text}>{comment.text}<br/><i>{comment.name}</i></div>)}
---
+++ js/InputForm.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class InputForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: {
+        name: '',
+        email: '',
+        rating: '',
+        text: ''
+      }
+    };
+    this.onClick = this.onClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  onClick() {
+    this.props.saveComment(this.state.input);
+    const input = {name: '', email: '', rating: '', text: ''};
+    this.setState({input});
+  }
+
+  handleChange(e) {
+    const field = e.target.name;
+    const value = e.target.value;
+    const input = this.state.input;
+    input[field] = value;
+    this.setState({input});
+  }
+
+  render() {
+    return (
+        <form id='comment-input'>
+          Name: <br/>
+          <input name="name" type="text" value={this.state.input.name} onChange={this.handleChange}/><br/>
+          Email: <br/>
+          <input name="email" type="text" value={this.state.input.email} onChange={this.handleChange}/><br/>
+          Rating: <br/>
+          <input name="rating" type="text" value={this.state.input.rating} onChange={this.handleChange}/><br/>
+          Comment: <br/>
+          <input name="text" type="text" value={this.state.input.text} onChange={this.handleChange}/><br/>
+          <input type="button" value="Submit" onClick={this.onClick}/>
+        </form>
+    );
+  }
+
+}
+
+InputForm.propTypes = {
+  saveComment: PropTypes.func.isRequired,
+};
+
+export default InputForm;
